e5431e218a36a1f38d63fb1111c4ff1cddfc6296
martyminton <marty.minton@gmail.com>
2013-12-10 18:01:28 -0500

    Initial Commit

--- gruntfile.js
+++ gruntfile.js
@@ -7,20 +7,18 @@ module.exports = function(grunt) {
             options: {
                 enabled: true,
                 max_jshint_notifications: 5,
-                title: "base" // Change this to your project
+                title: "base"
             }
         },
 
-        //- Uglify concatenated and other JS files
         uglify: {
             main : {
                 files: {
-                    'assets/js/main.min.js': ['build/js/main.js']
+                    'assets/js/min.js': 'build/js/main.js'
                 }
             }
         },
 
-        //- Compile SASS
         sass: {
             dist: {
                 options: {
@@ -33,7 +31,6 @@ module.exports = function(grunt) {
             }
         },
 
-        // Minify CSS
         cssmin: {
             minify: {
                 expand: true,
@@ -44,47 +41,44 @@ module.exports = function(grunt) {
             },
         },
 
-        //- Notify when task is complete
         notify: {
-            app_change: {
+            js_change: {
                 options: {
-                    title: 'Javascript',  // optional
-                    message: 'Concatenatated and minifed successfully', //required
+                    title: 'Javascript',  
+                    message: 'Concatenatated and minifed successfully',
                 }
             },
             css_complete: {
                 options: {
-                    title: 'SASS -> CSS',  // optional
-                    message: 'Compiled, prefixed, and moved successfully', //required
+                    title: 'SASS -> CSS',  
+                    message: 'Compiled, prefixed, and moved successfully', 
                 }
             },
         },
 
-        //- Watchers
         watch: {
             grunt: { 
                 files: ['gruntfile.js'],
-                tasks: ['default'], 
+                tasks: ['default']
             },
             sass: {
                 files: ['build/scss/**/*.scss'],
                 tasks: ['sass_change']
             },
             css: {
-                files: [/*'drupal/sites/all/themes/theme_name/css/*.css',*/ 'css/*.css'],
+                files: ['css/*.css'],
                 tasks: ['notify:css_complete', 'css_prefixed', 'css_min']
-            }/*,
+            },
             js: {
-                files: ['<%= concat.app.src %>', 'js/main.js'],
-                tasks: ['notify:app_change','app_change']                
-            }*/
+                files: ['build/js/main.js'],
+                tasks: ['notify:js_change','js_change']                
+            }
         }
     });
-    //- REGISTER ALL OUR GRUNT TASKS
+
     grunt.task.run('notify_hooks');
     grunt.registerTask('default', ['cssmin','sass','uglify', 'watch']);
-    grunt.registerTask('app_change', ['uglify:main']);
+    grunt.registerTask('js_change', ['uglify:main']);
     grunt.registerTask('sass_change', ['sass']);
     grunt.registerTask('css_min', ['cssmin']);
-    //grunt.registerTask('sync_files', ['sync']);
 };
